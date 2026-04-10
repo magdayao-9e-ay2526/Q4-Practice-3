2bedad8045dbda1d994fbcdfb805a10bb5e64ba1
Magdayao-9E-ay2526 <magdayao.ra@obmontessori.edu.ph>
2026-04-10 10:15:40 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+function displayLevelInfo() {
+    var levelentry = Number(document.getElementById('levelentry').value);
+        switch (levelentry) {
+            case 1:
+                document.getElementById("displayLevel").innerHTML=
+                "Level: Easy Mode";
+                break;
+            
+            case 2:
+                document.getElementById("displayLevel").innerHTML=
+                "Level: Normal Mode";
+                break;
+
+            case 3:
+                document.getElementById("displayLevel").innerHTML=
+                "Level: Difficult Mode";
+                break;
+
+            default:
+                window.alert("Invalid Section Number");
+              }
+        }
+    
