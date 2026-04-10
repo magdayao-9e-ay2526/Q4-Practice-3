f08b9ca46138974280d0b913fa81045f492ae56b
Magdayao-9E-ay2526 <magdayao.ra@obmontessori.edu.ph>
2026-04-10 10:21:31 +0800

    Update main

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function displayLevelInfo() {
     var levelentry = Number(document.getElementById('levelentry').value);
         switch (levelentry) {
@@ -21,3 +22,28 @@ function displayLevelInfo() {
               }
         }
     
+=======
+function displayLevelInfo() {
+    var levelentry = Number(document.getElementById('levelentry').value);
+        switch (levelentry) {
+            case 1:
+                document.getElementById("displayLevel").innerHTML=
+                "Level: Easy Mode";
+                break;
+            
+            case 2:
+                document.getElementById("displayLevel").innerHTML=
+                "Level: Normal Mode";
+                break;
+
+            case 3:
+                document.getElementById("displayLevel").innerHTML=
+                "Level: Difficult Mode";
+                break;
+
+            default:
+                window.alert("Invalid Section Number");
+              }
+        }
+    
+>>>>>>> Stashed changes
